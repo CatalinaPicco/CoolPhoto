731e84afab45f70353139e2f0cb5884bf9b9a695
Catalina <catalina.picco@gmail.com>
2019-09-26 17:27:12 -0300

    feat: add filter function to album list

--- app/src/main/java/com/example/coolphoto/adapters/NewsAdapter.java
+++ app/src/main/java/com/example/coolphoto/adapters/NewsAdapter.java
@@ -4,7 +4,8 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,16 +13,16 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.coolphoto.R;
 import com.example.coolphoto.models.Album;
-import com.example.coolphoto.ui.home.HomeFragment;
-import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
+import java.util.List;
 
 
-public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.NewsViewHolder> {
+public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.NewsViewHolder> implements Filterable {
 
     Context context;
     ArrayList<Album> albums;
+    ArrayList<Album> contactListFiltered;
 
     public NewsAdapter(Context context, ArrayList<Album> articles) {
         this.context = context;
@@ -47,6 +48,41 @@ public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.NewsViewHolder
         return albums.size();
     }
 
+    @Override
+    public Filter getFilter() {
+            return new Filter() {
+                @Override
+                protected FilterResults performFiltering(CharSequence charSequence) {
+                    String charString = charSequence.toString();
+                    if (charString.isEmpty()) {
+                        contactListFiltered = albums;
+                    } else {
+                        List<Album> filteredList = new ArrayList<>();
+                        for (Album row : albums) {
+
+                            // name match condition. this might differ depending on your requirement
+                            // here we are looking for name or phone number match
+                            if (row.title.toLowerCase().contains(charString.toLowerCase())) {
+                                filteredList.add(row);
+                            }
+                        }
+
+                        albums = (ArrayList<Album>) filteredList;
+                    }
+
+                    FilterResults filterResults = new FilterResults();
+                    filterResults.values = albums;
+                    return filterResults;
+                }
+
+                @Override
+                protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+                   albums = (ArrayList<Album>) filterResults.values;
+                   notifyDataSetChanged();
+                }
+            };
+        }
+
     public class NewsViewHolder extends RecyclerView.ViewHolder{
 
         TextView tvName;
@@ -59,4 +95,6 @@ public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.NewsViewHolder
             tvDesCription = itemView.findViewById(R.id.tvDescription);
         }
     }
+
+
 }
